09bc95ec1fa923197df9978ea810c7d667e4b7e7
su-mahesh <maheshkangude@gmail.com>
2021-01-15 23:17:13 +0530

    ADDED: validate last name

--- UserRegistration.java
+++ UserRegistration.java
@@ -3,31 +3,52 @@ import java.util.regex.Pattern;
 
 class UserRegistration{
 	private String firstName;
+	private String lastName;
 	
 	String firstNamePattern;
 	
-	public void setFirstName(){
+	public void setFirstLastName(boolean i){
 		Scanner sc = new Scanner(System.in);
 		boolean flag = true;
+		String name;
 		firstNamePattern = "[A-Z][a-z]{2}[a-z]*";
 		while(flag){
-		
-			System.out.println("Enter First Name");
-			firstName = sc.nextLine();
 			
-			if(!Pattern.matches(firstNamePattern, firstName)){
+			if(i){
+				System.out.println("Enter First Name");
+				name = firstName;		
+			}
+			else{
+				System.out.println("Enter Last Name");
+				name = lastName;		
+			}
+				name = sc.nextLine();
+			
+			if(!Pattern.matches(firstNamePattern, name)){
 				flag = true; 
 				System.out.println("wrong input");
 			}
-			else
-				flag = false; 
+			else{
+				lastName = name;
+				if(i)
+					firstName = name;
+				flag = false;
+				} 
 		}		
+	}
+	
+	public void setFirstName(){
+	setFirstLastName(true);
+	}
+	public void setLirstName(){
+	setFirstLastName(false);
 	}
 	
 	public static void main(String args[]){
 	
 		UserRegistration UserReg = new UserRegistration();
 		
-		UserReg.setFirstName();		
+		UserReg.setFirstName();
+		UserReg.setLirstName();	
 	}
 }
