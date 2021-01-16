3b0823be639463c3abbf715d8f420945cc349429
su-mahesh <maheshkangude@gmail.com>
2021-01-16 17:21:30 +0530

    ADDED: email validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,44 +4,70 @@ import java.util.regex.Pattern;
 class UserRegistration{
 	private String firstName;
 	private String lastName;
+	private String email;
+	String str = " ";
+	boolean check = true;
 	
-	String firstNamePattern;
+	String firstLastNamePattern = "[A-Z][a-z]{2}[a-z]*";
+	String emailPattern = "[A-Za-z0-9_][A-Za-z0-9_.]{5,64}[@][A-Za-z0-9-]{2,}[.][A-Za-z]{1,63}([.][A-Za-z]{2})?";
 	
-	public void setFirstLastName(boolean i){
-		Scanner sc = new Scanner(System.in);
-		boolean flag = true;
-		String name;
-		firstNamePattern = "[A-Z][a-z]{2}[a-z]*";
+	Scanner sc = new Scanner(System.in);
+	
+	public boolean checkName(String str){	
+	
+	return Pattern.matches(firstLastNamePattern, str) ? true : false;
+		//	if(Pattern.matches(firstLastNamePattern, str))
+	//			retun true;
+		//	else
+			//	return false;						
+	}
+	
+	public void setFirstName(){		
+		while(check){	
+			System.out.println("Enter First Name");
+			str = sc.nextLine();
+						
+			if(checkName(str)){
+				firstName = str;
+				check = false;
+			}
+			else
+				System.out.println("wrong format");				
+		}
+		check = true;
+	}
+	
+	public void setLastName(){
+		while(check){
+			System.out.println("Enter Last Name");
+			str = sc.nextLine();
+						
+			if(checkName(str)){
+				lastName = str;
+				check = false;
+			}
+			else
+				System.out.println("wrong format\n");				
+		}
+		check = true;
+	}
+	
+	public void setEmail(){
+		boolean flag = true;				
+		String str;
+		
 		while(flag){
+					
+				System.out.println("Enter email");
+				str = sc.nextLine();;					
 			
-			if(i){
-				System.out.println("Enter First Name");
-				name = firstName;		
-			}
-			else{
-				System.out.println("Enter Last Name");
-				name = lastName;		
-			}
-				name = sc.nextLine();
-			
-			if(!Pattern.matches(firstNamePattern, name)){
-				flag = true; 
+			if(!Pattern.matches(emailPattern, str)){
+				flag = true;
 				System.out.println("wrong input");
 			}
-			else{
-				lastName = name;
-				if(i)
-					firstName = name;
-				flag = false;
-				} 
-		}		
-	}
-	
-	public void setFirstName(){
-	setFirstLastName(true);
-	}
-	public void setLirstName(){
-	setFirstLastName(false);
+			else
+				flag = false;			 
+		}				
 	}
 	
 	public static void main(String args[]){
@@ -49,6 +75,8 @@ class UserRegistration{
 		UserRegistration UserReg = new UserRegistration();
 		
 		UserReg.setFirstName();
-		UserReg.setLirstName();	
+		UserReg.setLastName();
+		UserReg.setEmail();
+			
 	}
 }
